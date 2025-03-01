5e4ecc2e2fff9c5b97018dfc498ab1333bf1e084
rinaldi <rinaldiwaaw91@gmail.com>
2025-03-01 11:35:00 +0700

    Deploy Vercel

--- config/db.js
+++ config/db.js
@@ -1,13 +1,31 @@
+// const mysql = require('mysql2');
+
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'rbac_db',
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0
+// });
+
+// module.exports = pool.promise();
+
+
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'rbac_db',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: process.env.DB_SSL === 'true' ? {rejectUnauthorized: true} : false,
+  connectionLimit: 10
 });
 
-module.exports = pool.promise();
+const promisePool = pool.promise();
+
+module.exports = promisePool;
